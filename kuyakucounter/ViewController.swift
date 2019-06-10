7bb7d97f0ff6cd5343ed7c04ce08a89918bb5a9c
Yuhi-Kikuchi620409 <snow.1ove-1ife-_-0409@ezweb.ne.jp>
2019-06-10 09:13:52 +0900

    Initial Commit

---
+++ kuyakucounter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kuyakucounter
+//
+//  Created by 菊地雄飛 on 2019/06/10.
+//  Copyright © 2019 菊地雄飛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
